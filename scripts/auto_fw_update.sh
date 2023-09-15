24417bf116a82cf60d26fd7af093f6c040fd1c11
Sunghoon Cho <sunghoonism@gmail.com>
2023-09-15 16:24:55 +0900

    edit fermware update script

--- scripts/auto_fw_update.sh
+++ scripts/auto_fw_update.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-export NOW_VER=`cat ../../version | sed 's/\.//g'`
+export SCRIPT_DIR="/home/bssoft/home-noise-speaker-2301/scripts"
+cd $SCRIPT_DIR
+export NOW_VER=`cat ../../version`
 export RESPONSE=`curl https://home-therapy.bs-soft.co.kr/api/firmware/recent-ver | grep result`
 
 if [ "$RESPONSE" != "" ]; then
     export TARGET_VER=`echo $RESPONSE | cut -d"\"" -f4 | sed 's/\.//g'`
     if [ "$NOW_VER" -lt "$TARGET_VER" ]; then
         echo "Update firmware"
-        curl https://home-therapy.bs-soft.co.kr/api/firmware/update/$NOW_VER | bash 
+        curl https://home-therapy.bs-soft.co.kr/api/firmware/update/$NOW_VER | bash &> log.txt
+        cd $SCRIPT_DIR
+        export RESULT=`tail -1 log.txt`
+        if [ "$RESULT" = "ok" ]; then
+            cat  <<EOF > ../../version 
+$TARGET_VER
+EOF
+        fi
     else
         echo "No update"
     fi
