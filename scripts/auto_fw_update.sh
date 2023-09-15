3aac5481ce1297613298c70e4532b75329b7e3ed
Sunghoon Cho <sunghoonism@gmail.com>
2023-09-15 17:56:03 +0900

    remove version file edit script

--- scripts/auto_fw_update.sh
+++ scripts/auto_fw_update.sh
@@ -2,6 +2,8 @@
 
 export SCRIPT_DIR="/home/bssoft/home-noise-speaker-2301/scripts"
 cd $SCRIPT_DIR
+
+export SN=`cat /boot/bssoft/id.txt`
 export NOW_VER=`cat ../../version`
 export RESPONSE=`curl https://home-therapy.bs-soft.co.kr/api/firmware/recent-ver | grep result`
 
@@ -9,13 +11,11 @@ if [ "$RESPONSE" != "" ]; then
     export TARGET_VER=`echo $RESPONSE | cut -d"\"" -f4 | sed 's/\.//g'`
     if [ "$NOW_VER" -lt "$TARGET_VER" ]; then
         echo "Update firmware"
-        curl https://home-therapy.bs-soft.co.kr/api/firmware/update/$NOW_VER | bash &> log.txt
+        curl https://home-therapy.bs-soft.co.kr/api/firmware/update/$NOW_VER/$SN | bash &> log.txt
         cd $SCRIPT_DIR
         export RESULT=`tail -1 log.txt`
         if [ "$RESULT" = "ok" ]; then
-            cat  <<EOF > ../../version 
-$TARGET_VER
-EOF
+            curl https://home-therapy.bs-soft.co.kr/api/firmware/history/$TARGET_VER/$SN
         fi
     else
         echo "No update"
